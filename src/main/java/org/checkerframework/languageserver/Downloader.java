d52c8ec36e4417eaaf3c24b6be574c3a710531c0
zhangjiangqige <zhangjiangqige@gmail.com>
2019-09-18 11:26:37 -0400

    retrieves the latest release

--- src/main/java/org/checkerframework/languageserver/Downloader.java
+++ src/main/java/org/checkerframework/languageserver/Downloader.java
@@ -2,14 +2,18 @@ package org.checkerframework.languageserver;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import net.lingala.zip4j.ZipFile;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 
 public class Downloader {
     public static File downloadLanguageServer(File folder) throws IOException {
@@ -22,22 +26,20 @@ public class Downloader {
         return Paths.get(FilenameUtils.getFullPath(cfzip), FilenameUtils.getBaseName(cfzip)).toFile();
     }
 
-    public static URL getLanguageServerURL() {
-        // TODO
-        try {
-            return new URL("https://github.com/zhangjiangqige/checker-framework-languageserver/releases/latest/download/checker-framework-languageserver-all-0.0.1.jar");
-        } catch (MalformedURLException e) {
-            return null;
-        }
+    public static URL getLanguageServerURL() throws IOException {
+        return getLatestGithubRelease("zhangjiangqige", "checker-framework-languageserver");
     }
 
-    public static URL getCheckerFrameworkURL() {
-        // TODO
-        try {
-            return new URL("https://github.com/typetools/checker-framework/releases/latest/download/checker-framework-2.11.0.zip");
-        } catch (MalformedURLException e) {
-            return null;
-        }
+    public static URL getCheckerFrameworkURL() throws IOException {
+        return getLatestGithubRelease("typetools", "checker-framework");
+    }
+
+    private static URL getLatestGithubRelease(String author, String repo) throws IOException {
+        String json = IOUtils.toString(new URL("https://api.github.com/repos/" + author + "/" + repo + "/releases/latest"), StandardCharsets.UTF_8.name());
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(json);
+        JsonArray assets = root.getAsJsonObject().getAsJsonArray("assets");
+        return new URL(assets.get(0).getAsJsonObject().get("browser_download_url").getAsString());
     }
 
     private static File download(URL url, File folder) throws IOException {
